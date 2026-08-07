c0234714c7e43ba8abc603cd4ee4b5959e211e68
Nitisha Punmiya <punmiyanitisha16406@gmail.com>
2026-08-08 02:52:52 +0530

    Update Failed_ssh_login_detector.sh

--- Bash /Failed_ssh_login_detector.sh
+++ Bash /Failed_ssh_login_detector.sh
@@ -1,5 +1,6 @@
 #this script is to detect failed ssh login attemps .
 #it extracts the ip and print the number of times ip is attemting to performe brute force
+#if the failed attemps exceed 10 then print ip
 
 
 
@@ -10,4 +11,4 @@ echo "====Failed SSH logins===="
 grep  'Failed password'  /var/log/auth.log | \
 awk '{print  $(NF-3)}' | \
 sort | uniq -c | sort -nr | \
-awk '{printf "%-15s : %s attempts\n", $2, $1}'
+awk '$1>10 {printf "%-15s : %s attempts\n", $2, $1}'
